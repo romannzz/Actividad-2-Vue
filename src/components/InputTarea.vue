1da7c5d491e3ea191a677af170320ef645e96824
VictorRoman <r0m4nv1ct0r@gmail.com>
2024-11-19 20:04:22 +0100

    elimine dos apartados en el InputTarea que cambiare mas adelante

--- src/components/InputTarea.vue
+++ src/components/InputTarea.vue
@@ -6,16 +6,7 @@
 <template>
 
 
-<div>
 
-    <label>USUARIO</label>
-    <input type="TAREA" v-model="sTarea" > </input>
-
-</div>
-
-
-<button @click="presioneAdTarea">AÑADIR TAREA</button>
-<button @click="presioneEliminar">ELIMINAR TAREA</button>
 </template>
 
 <style scoped>
