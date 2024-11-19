455c61f40c743c3b9a6ab540ff541f63c473ebc8
VictorRoman <r0m4nv1ct0r@gmail.com>
2024-11-19 18:49:12 +0100

    añadi dos tareas

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,27 @@
 import InputTarea from './components/InputTarea.vue';
 import Todolist from './components/Todolist.vue';
 
+export default {
+
+name: 'App',
+components : {
+    Todolist,
+    InputTarea,
+},
+data(){
+    return {
+            tareas: [
+                {id:1, titulo: 'Deberes de Vue', completada:false},
+                {id:2, 'Comprar la copa para los premios celo 2024-2025', completada:false},
+            ],
+
+    }
+
+
+}
+
+}
+
 </script>
 
 <template>
